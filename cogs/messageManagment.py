7b505731db9014e59e4be073e5b44c7ea6175e1d
Tyler Spurr <tspurrcoding@gmail.com>
2020-08-12 15:16:05 -0400

    Introduction to Cogs Created a cogs folder and moved messageManagment.py to the folder. Integrated the cogs folder into the main file northBot.py. Edited the commands in messageManagment.py to fit the styling of cogs.

---
+++ cogs/messageManagment.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+
+
+class messageManagment(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    # Event Showing messageManagment is loaded
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('loaded messageManagment')
+
+    # Saying Hello to anyone who wants to say hi
+    @commands.command()
+    async def hello(self, ctx):
+        await ctx.send('Hello')
+
+    # Deleting all TEXT messages in a specific channel
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if str(message.channel) == "pics" and message.content != "":
+            await message.channel.purge(limit=1)
+
+    # Deleting a specific NUMBER of messages anywhere
+    @commands.command()
+    async def delete(self, ctx, arg):
+        await ctx.channel.purge(limit=int(arg))
+
+
+def setup(client):
+    client.add_cog(messageManagment(client))
+
--- northBot.py
+++ northBot.py
@@ -1,14 +1,26 @@
 import discord
-import messageManagment
+import os
+from discord.ext import commands
 
 # Turns the Bot Online
-client = discord.Client()
+client = commands.Bot(command_prefix = '.')
 
 # On Startup the bot prints that it has logged in
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+@client.command()
+async def load(ctx, extension):
+    client.load_extension('cogs.' + extension)
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension('cogs.' + extension)
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension('cogs.' + (filename[:-3]))
 
 # Token to Identify which bot is being called, and running the code
 client.run('NzQzMTQyNjI1Mjk2Nzc3MzI3.XzQXgA.qS-jl0p8OYGlnUki0ff8244wjIw')
