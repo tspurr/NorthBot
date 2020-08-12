80db8ccd0405d718ffa71351f82ead40c3c8b4e0
Tyler Spurr <tspurrcoding@gmail.com>
2020-08-12 13:53:14 -0400

    Initial commit

---
+++ northBot.py
@@ -0,0 +1,13 @@
+import discord
+import messageManagment
+
+# Turns the Bot Online
+client = discord.Client()
+
+# On Startup the bot prints that it has logged in
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+# Token to Identify which bot is being called, and running the code
+client.run('NzQzMTQyNjI1Mjk2Nzc3MzI3.XzQXgA.qS-jl0p8OYGlnUki0ff8244wjIw')
