09c20883325824ff3228921a52c2eb44913ad174
Tyler Spurr <tspurrcoding@gmail.com>
2020-08-28 22:38:35 -0400

    MIT and serverStats stuff

--- cogs/messageManagement.py
+++ cogs/messageManagement.py
@@ -1,3 +1,27 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2020 Tyler Spurr
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
+
 import discord
 from discord.ext import commands
 from pymongo import MongoClient
@@ -89,6 +113,7 @@ class messageManagement(commands.Cog, name='Message Management'):
 
     # Deleting a specific NUMBER of messages anywhere
     @commands.command()
+    @commands.has_permissions(manage_messages=True)
     async def delete(self, ctx, arg):
         if ctx.message.author.guild_permissions.administrator:  # Have to be an administrator to run
             if int(arg) > 100:
@@ -102,6 +127,7 @@ class messageManagement(commands.Cog, name='Message Management'):
     # Turning on/off message restrictions
     # TODO Make this command administrator only
     @commands.command()
+    @commands.has_permissions(administrator=True)
     async def profanityFilter(self, ctx, onOff):
         serverID = ctx.guild.id
 
